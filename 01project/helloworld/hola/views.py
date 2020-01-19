a4bf304e92b60f3c6853c2bb48f49df1301b8f9f
shivam-bit <shivama5tronot@gmail.com>
2020-01-19 19:10:53 +0530

    implemented hola app as home view

--- 01project/helloworld/hola/views.py
+++ 01project/helloworld/hola/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def HomePageView(request):
+    return HttpResponse('hola-means born on holi')
